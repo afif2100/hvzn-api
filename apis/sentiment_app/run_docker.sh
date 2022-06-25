cfacb68c6e7fa564eb2c171f43296f105572bcee
tokopedia-hafizhan <hafizhan.aliady@tokopedia.com>
2022-06-25 15:58:20 +0700

    edit docker

--- apis/sentiment_app/run_docker.sh
+++ apis/sentiment_app/run_docker.sh
@@ -3,7 +3,7 @@ export REGISTRY=asia.gcr.io/hvzn-development/afif2100/sentiment-app
 
 
 # if build
-docker build -t ${REGISTRY}:latest .
+docker build --no-cache -t ${REGISTRY}:latest .
 # if push
 docker push ${REGISTRY}:latest
 # if run
