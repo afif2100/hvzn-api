0609ded8375d96307b86eefcdbc88807a2a94879
tkpd-hafizhan <hafizhan.aliady@tokopedia.com>
2022-10-24 04:02:16 +0000

    edit homepage

--- apis/streamlit/Home.py
+++ apis/streamlit/Home.py
@@ -13,15 +13,16 @@ st.write(
 """
 )
 
-image = Image.open("dp.jpg")
+image = Image.open("images/dp.jpg")
 st.image(image, width=150)
 
 st.markdown("## Summary", unsafe_allow_html=True)
 st.info(
     """
-- Experienced Educator, Researcher and Administrator with almost twenty years of experience in data-oriented environment and a passion for delivering insights based on predictive modeling. 
-- Strong verbal and written communication skills as demonstrated by extensive participation as invited speaker at `10` conferences as well as publishing 149 research articles.
-- Strong track record in scholarly research with H-index of `32` and total citation of 3200+.
+Hello, I'm Hafizhan Aliady, I am a person who works on data, or what we usually call Data Science. 
+I am a person who has a Speciality in Machine Learning engineering and MLOps \n
+Have experience in building machine learning platforms to support data scientists deploying their model/products to become something that can make an impact.
+I'm happy to help the data science team to increase their productivity through machine learning engineering.
 """
 )
 
@@ -52,7 +53,7 @@ st.markdown(
         <a class="nav-link" href="#work-experience">Work Experience</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#bioinformatics-tools">Bioinformatics Tools</a>
+        <a class="nav-link" href="#bioinformatics-tools">Tools</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="#social-media">Social Media</a>
@@ -108,28 +109,15 @@ st.markdown(
 )
 
 txt(
-    "**Doctor of Philosophy** (Medical Technology), *Mahidol University*, Thailand",
-    "2002-2006",
-)
+    "**Bachelor of Statistics** (Data Science), *Universitas Islam Indonesia*, D.I Yogyakarta, Indonesia",
+"")
 st.markdown(
     """
-- GPA: `3.89`
-- Research thesis entitled `Computer-aided molecular design for biological and chemical applications : Quantum chemical and machine learning approach`.
-- Received Royal Golden Jubilee Ph.D. Scholarship of `2.152 million THB` covering tuition and stipend.
+- GPA: `3.64`
 - Thesis awarded `1st` Prize by the National Research Council of Thailand.
 """
 )
 
-txt(
-    "**Bachelors of Science** (Biological Science), *Mahidol University International College*, Thailand",
-    "1998-2002",
-)
-st.markdown(
-    """
-- GPA: `3.65`
-- Graduated with First Class Honors.
-"""
-)
 
 #####################
 st.markdown(
@@ -163,61 +151,6 @@ txt(
     "**Lecturer**, Faculty of Medical Technology, Mahidol University, Thailand",
     "2006-2009",
 )
-st.markdown(
-    """
-- Provided mentorship and supervision to junior faculty, researchers, Ph.D./M.Sc./B.Sc. students. Mentored `3` Post-doctoral fellows, supervised `13` Ph.D. students, supervised `8` M.Sc. students, supervised `13` B.Sc. students and hosted `6` visiting students from U.S., Sweden and India.
-- Wrote and applied for research grants. Served as Principal Investigator for research grants that have been awarded `12.5 million THB` and `1.117 million SEK` in research funding from Thai and Swedish grant agencies.
-- Conducted research by applying machine learning to computational drug discovery and ensuring that research output exceeds `20+` articles per year.
-- Taught `10+` undergraduate/graduate classes on Bioinformatics, Data Mining, Scientific Research and Presentation, Research Methodology, Graduate Seminar, Programming for Health Data Science, etc.
-- Peer reviewed `100+` research articles for leading scientific journals.
-"""
-)
-
-txt(
-    "**Co-Chair**, International Conference on Pharmaceutical Bioinformatics at Pattaya, Thailand",
-    "2016",
-)
-st.markdown(
-    """
-- Oversee all aspects of the conference preparations from conception to launch. This include inviting keynote and plenary speakers, create advertisement flyers, create abstract book, etc.
-- Conference attracted `200+` participants from `40+` countries from university and industry sector.
-- Chaired keynote session, technical workshop and some of the parallel sessions.
-"""
-)
-
-txt(
-    "**Content Creator**, [Data Professor YouTube Channel](https://youtube.com/dataprofessor/)",
-    "2019-Present",
-)
-st.markdown(
-    """
-- `100,000+` subscribers on YouTube
-- Created `261` educational videos on data science, machine learning and bioinformatics as well as hosted several podcast episodes with data scientists.
-- Created `3` sponsored videos for Notion, Gradio and Classpert.
-"""
-)
-
-txt(
-    "**Content Creator**, [Coding Professor YouTube Channel](https://youtube.com/codingprofessor/)",
-    "2019-Present",
-)
-st.markdown(
-    """
-- `3,200+` subscribers on YouTube
-- Created `38` educational videos on Python and R programming.
-"""
-)
-
-txt(
-    "**Technical Writer**, [Data Professor Blog](https://data-professor.medium.com/) on Medium.com",
-    "2019-Present",
-)
-st.markdown(
-    """
-- `4,100+` subscribers on Medium
-- Written `68` technical blogs on data science, machine learning and bioinformatics.
-"""
-)
 
 #####################
 st.markdown(
@@ -235,82 +168,6 @@ txt4(
     "An automated data mining software based on Weka",
     "http://www.mt.mahidol.ac.th/autoweka/",
 )
-txt4(
-    "ACPred",
-    "A computational tool for the prediction and analysis of anticancer peptides",
-    "http://codes.bio/acpred/",
-)
-txt4(
-    "BioCurator",
-    "A web server for curating ChEMBL bioactivity data",
-    "http://codes.bio/biocurator/",
-)
-txt4(
-    "CryoProtect",
-    "A web server for classifying antifreeze proteins from non-antifreeze proteins",
-    "http://codes.bio/cryoprotect/",
-)
-txt4(
-    "ERpred",
-    "A web server for the prediction of subtype-specific estrogen receptor antagonists",
-    "http://codes.bio/erpred",
-)
-txt4(
-    "HCVpred",
-    "A web server for predicting the bioactivity of Hepatitis C virus NS5B inhibitors",
-    "http://codes.bio/hemopred/",
-)
-txt4(
-    "HemoPred",
-    "A web server for predicting the hemolytic activity of peptides",
-    "http://codes.bio/hemopred/",
-)
-txt4(
-    "iQSP",
-    "A sequence-based tool for the prediction and analysis of quorum sensing peptides",
-    "http://codes.bio/iqsp/",
-)
-txt4(
-    "Meta-iAVP",
-    "A sequence-based meta-predictor for improving the prediction of antiviral peptides",
-    "http://codes.bio/meta-iavp/",
-)
-txt4(
-    "osFP",
-    "A web server for predicting the oligomeric state of fluorescent proteins",
-    "http://codes.bio/osfp/",
-)
-txt4(
-    "PAAP",
-    "A web server for predicting antihypertensive activity of peptides",
-    "http://codes.bio/paap/",
-)
-txt4(
-    "PepBio",
-    "A web server for predicting the bioactivity of host defense peptide",
-    "http://codes.bio/pepbio",
-)
-txt4(
-    "PyBact",
-    "Open source software written in Python for bacterial identification",
-    "https://sourceforge.net/projects/pybact/",
-)
-txt4(
-    "TargetAntiAngio",
-    "A sequence-based tool for the prediction and analysis of anti-angiogenic peptides",
-    "http://codes.bio/targetantiangio/",
-)
-txt4(
-    "ThalPred",
-    "Development of decision model for discriminating Thalassemia trait and Iron deficiency anemia",
-    "http://codes.bio/thalpred/",
-)
-txt4(
-    "THPep",
-    "A web server for predicting tumor homing peptides",
-    "http://codes.bio/thpep/",
-)
-
 
 #####################
 st.markdown(
@@ -335,13 +192,5 @@ st.markdown(
 ## Social Media
 """
 )
-txt2("LinkedIn", "https://www.linkedin.com/in/chanin-nantasenamat")
-txt2("Twitter", "https://twitter.com/thedataprof")
-txt2("GitHub", "https://github.com/chaninn/")
-txt2("", "https://github.com/chaninlab/")
-txt2("", "https://github.com/dataprofessor")
-txt2("ORCID", "http://orcid.org/0000-0003-1040-663X")
-txt2("Scopus", "http://www.scopus.com/authid/detail.url?authorId=12039071300")
-txt2("ResearcherID", "http://www.researcherid.com/rid/F-1021-2010")
-txt2("ResearchGate", "https://www.researchgate.net/profile/Chanin_Nantasenamat")
-txt2("Publons", "https://publons.com/a/303133/")
+txt2("LinkedIn", "https://www.linkedin.com/in/hafizhan-aliady")
+txt2("GitHub", "https://github.com/afif2100/")
