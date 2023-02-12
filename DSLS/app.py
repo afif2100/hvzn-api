8e8c0d4b4ab5b4e4be9753f08a5ade0fc0bc70dc
afif2100 <hafizhan.afif@gmail>
2023-02-12 11:21:47 +0700

    update

--- DSLS/app.py
+++ DSLS/app.py
@@ -1,25 +1,53 @@
-#app.py
+# app.py
 from fastapi import FastAPI, Body, Request
+from pydantic import BaseModel
 
 
 # definisi dari app
 app = FastAPI()
 
 
-#membuat endpoint home, dengan method get
+# membuat endpoint home, dengan method get
 @app.get("/")
 def home():
-    return {"msg":"hello world"}
+    return {"msg": "hello world"}
+
 
 @app.get("/health")
-async def health_check():
+def health_check():
     return {"status": "healthy"}
 
-class PredictSalary():
-    
-    def __init__(self) -> None:
-        self.base_salary = 5000000
-        self.age_salary_multiplier = 50000
-        
-    def calculate_salary(self, age):
-        return self.base_salary + (age * self.age_salary_multiplier)
+
+# membuat data model
+class Person(BaseModel):
+    name: str = "Fulan"
+
+
+@app.post("/hello")
+def hello(person: Person):
+    return f"Hello, {person.name}"
+
+
+# # Membuat Prediction model
+# class PredictSalary:
+#     def __init__(self) -> None:
+#         self.base_salary = 5000000
+#         self.age_salary_multiplier = 50000
+
+#     def calculate_salary(self, age):
+#         return self.base_salary + (age * self.age_salary_multiplier)
+
+
+# ps = PredictSalary()
+
+# # membuat data model
+# class Person(BaseModel):
+#     # nama variabel:tipe data
+#     name: str
+#     age: int
+
+
+# @app.post("/predict")
+# def predict_salary(employee: Person):
+#     salary = ps.calculate_salary(employee.age)
+#     return {"name": employee.name, "salary": salary}
