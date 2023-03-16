63b8f456da8be0db8b5b2afcf1602596edf6cebe
tkpd-hafizhan <hafizhan.aliady@tokopedia.com>
2023-03-16 08:07:34 +0700

    add logger

--- apis/fastapi_boilerplate/app.py
+++ apis/fastapi_boilerplate/app.py
@@ -1,36 +1,46 @@
 from fastapi import FastAPI, Request, Body
 import joblib
 import numpy as np
-import json
+from loguru import logger
+
 
 app = FastAPI()
 
 
 @app.get("/")
 async def read_main():
-    return {"message":"Hello World"}
+    logger.info("OK")
+    logger.debug("OK")
+    logger.warning("OK")
+    logger.error("OK")
+    logger.critical("OK")
+    return {"message": "Hello World"}
+
 
 @app.get("/health")
 async def health_check():
     return {"status": "healthy"}
 
+
 @app.post("/predict")
 async def predict(payload: dict = Body(...)):
 
     # Get body content
-    data = payload['data']
+    data = payload["data"]
     data = np.array([data])
 
     # Load the model
-    model = joblib.load('model/model.pkl')
+    model = joblib.load("model/model.pkl")
 
     # Make the prediction
     prediction = model.predict(data)
 
     if prediction[0] == 1:
-        result = 'Diabetes'
+        result = "Diabetes"
     else:
-        result = 'Not Diabetes'
+        result = "Not Diabetes"
 
     # Return the prediction
-    return {"prediction": result}
+    payload = {"prediction": result}
+    logger.info(payload)
+    return payload
